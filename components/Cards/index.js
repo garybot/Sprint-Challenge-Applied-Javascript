0ed95d4ba82b2e7cf98020ea3fcb2c0b6683d9a2
garybot <argot.pryer@gmail.com>
2019-09-15 19:09:14 -0700

    comepleted article card generator.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,19 +1,39 @@
-// STEP 3: Create Article cards.
-// -----------------------
-// Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
-// You will be creating a component for each 'article' in the list.
-// This won't be as easy as just iterating over an array though.
-// Create a function that will programmatically create the following DOM component:
-//
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {authors name}</span>
-//   </div>
-// </div>
-//
-// Create a card for each of the articles and add the card to the DOM.
+
+const cards = document.querySelector('.cards-container');
+
+function createCard(article) {
+  const card = document.createElement('div');
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const imageDiv = document.createElement('div');
+  const image = document.createElement('img');
+  const name = document.createElement('span');
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imageDiv);
+  imageDiv.appendChild(image);
+  author.appendChild(name);
+
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imageDiv.classList.add("img-container");
+
+  headline.textContent = article.headline
+  name.textContent = `By ${article.authorName}`;
+  image.src = article.authorPhoto;
+
+  return card;
+}
+
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    const articleObj = response.data.articles
+    for (key in articleObj) {
+      for (article in articleObj[key]) {
+        cards.appendChild(createCard(articleObj[key][article]));
+      }
+    }
+  });
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -3,7 +3,6 @@ const topics = document.querySelector('.topics');
 
 axios.get("https://lambda-times-backend.herokuapp.com/topics")
   .then(response => {
-    console.log(response);
     response.data.topics.forEach(topic => {
       const tab = document.createElement('div');
       tab.classList.add('tab');
