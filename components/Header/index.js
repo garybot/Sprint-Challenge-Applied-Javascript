d785fcc16347b8e4fa36d308fb10e77287d4b805
garybot <argot.pryer@gmail.com>
2019-09-15 18:30:57 -0700

    Wrote Header maker function.

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,12 +1,23 @@
-// STEP 1: Create a header component.
-// -----------------------
-// Using a function create the component you see below:
-//
-//  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
-//    <h1>Lambda Times</h1>
-//    <span class="temp">98°</span>
-//  </div >
-// And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  const header = document.createElement('div');
+  const date = document.createElement('span');
+  const heading = document.createElement('h1');
+  const temp = document.createElement('span');
+
+  header.appendChild(date);
+  header.appendChild(heading);
+  header.appendChild(temp);
+
+  header.classList.add("header");
+  date.classList.add("date");
+  temp.classList.add("temp");
+
+  date.textContent = "SMARCH 28, 2019";
+  heading.textContent = "Lambda Times";
+  temp.textContent = "98°";
+
+  return header;
+}
+
+document.getElementsByClassName('header-container')[0].appendChild(Header());
