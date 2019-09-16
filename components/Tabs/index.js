243cb344a1df85dcb9164db7a61d642fb8c0a03f
garybot <argot.pryer@gmail.com>
2019-09-15 18:38:09 -0700

    wrote axios call and added trending topics tabs.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,9 +1,13 @@
-// Step 2: Create Tabs
-// -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
-// Once the data is returned console.log it and review the structure.
-// Iterate over the topics creating a new Tab component and add it to the DOM
-// under the .topics element.
-//
-//  The tab component should look like this:
-//    <div class="tab">topic here</div>
+
+const topics = document.querySelector('.topics');
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response);
+    response.data.topics.forEach(topic => {
+      const tab = document.createElement('div');
+      tab.classList.add('tab');
+      tab.textContent = topic;
+      topics.appendChild(tab);
+    })
+  });
